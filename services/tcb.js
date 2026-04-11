fc9cd0ba6e3ef87f5e51b88c0541047d6ed23b5e
Suntoryyyy <2748033822@qq.com>
2026-04-11 20:13:19 +0800

    chore(db): swap firebase for tencent cloudbase to support mainland users

---
+++ services/tcb.js
@@ -0,0 +1,14 @@
+import cloudbase from '@cloudbase/js-sdk';
+
+// We will initialize your Tencent CloudBase app here once you give me the Environment ID
+/*
+export const app = cloudbase.init({
+  env: 'YOUR-ENV-ID' // e.g., 'siderun-123456'
+});
+
+export const auth = app.auth();
+export const db = app.database();
+*/
+
+// Placeholder until you grab the keys
+export default cloudbase;
