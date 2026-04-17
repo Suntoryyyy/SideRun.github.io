bd4696a55aa2e91f3a422ea8493b43409f39c57d
Suntoryyyy <2748033822@qq.com>
2026-04-17 23:13:10 +0800

    clean patch files

--- patch-comma.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('styles/FriendsScreenStyles.js', 'utf8');
-code = code.replace(/fontSize: 24,\n    }\n\n  feedCard: \{/g, 'fontSize: 24,\n    },\n  feedCard: {');
-fs.writeFileSync('styles/FriendsScreenStyles.js', code);
--- patch-feed.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('components/ActivityFeed.js', 'utf8');
-code = code.replace(/}\)\)\}/, '}))\n      )}');
-fs.writeFileSync('components/ActivityFeed.js', code);
