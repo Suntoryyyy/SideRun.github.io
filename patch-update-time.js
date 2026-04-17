1b7192e1f6d3fd91145b6fe5c4802c4c08a842c7
Suntoryyyy <2748033822@qq.com>
2026-04-17 23:20:27 +0800

    clean patch files

--- patch-update-time.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('hooks/useRunTracking.js', 'utf8');
-
-const rxInsert = /          setCurrentLocation\(newLoc\);/;
-code = code.replace(rxInsert, '          setCurrentLocation(newLoc);\n          lastUpdateTime.current = Date.now();');
-fs.writeFileSync('hooks/useRunTracking.js', code);
